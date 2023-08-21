99f6b689bffa1a5ccf03c15e6fec391eccea1fba
serhiikhkr <05hello.world11@gmail.com>
2023-08-21 16:06:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -95,46 +95,86 @@ class Record:
             return days_to_birthday
         return days_to_birthday
 
+    def __str__(self):
+        return f"{self.name.value} {[ph.value for ph in self.phones]} {self.birthday.value.date()}"
+
 
 class AddressBook(UserDict):
+    N = 0
+
     def add_record(self, record: Record):
         self.data[record.name.value] = record
 
-    def iterator(self, quantity):
-        return Iterator(quantity)
-
-
-class Iterator:
-    def __init__(self, quantity):
-        self.quantity = quantity
-        self.start = 0
+    def iterator(self, n=None):
+        if n:
+            AddressBook.N = n
+        return self.__next__()
 
     def __iter__(self):
-        return self
+        temp_lst = []
+        counter = 0
+
+        for var in self.data.values():
+            temp_lst.append(var)
+            counter += 1
+            if counter >= AddressBook.N:
+                yield temp_lst
+                temp_lst.clear()
+                counter = 0
+        yield temp_lst
 
     def __next__(self):
-        self.start += 1
-        if self.start <= self.quantity:
-            return self
-        else:
-            raise StopIteration
+        generator = self.__iter__()
+        page = 1
+        while True:
+            user_input = input("Press ENTER")
+            if user_input == "":
+                try:
+                    result = next(generator)
+                    if result:
+                        print(f"{'*' * 20} Page {page} {'*' * 20}")
+                        page += 1
+                    for var in result:
+                        print(var)
+                except StopIteration:
+                    print(f"{'#' * 20} END {'#' * 20}")
+                    break
+            else:
+                break
 
-""" Как сделать метод ierator что бы возвращал генератор вообще не пойму"""
 
-# if __name__ == '__main__':
-#     name = Name('Bill')
-#     phone = Phone('1234567890')
-#     birth = Birthday('11.05.1995')
-#     rec = Record(name, phone, birth)
-#
-#     ab = AddressBook()
-#     ab.add_record(rec)
-#     name = Name('Seee')
-#     phone = Phone('989898989')
-#     birth = Birthday('20.11.1995')
-#     rec = Record(name, phone, birth)
-#     ab.add_record(rec)
-#     print(ab.data[name.value].birthday.value)
-#     print(ab)
-#     for i in ab.iterator(1):
-#         print(i)
+if __name__ == '__main__':
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    birth = Birthday('11.05.1995')
+    rec = Record(name, phone, birth)
+
+    ab = AddressBook()
+    ab.add_record(rec)
+
+    name1 = Name('Seee1')
+    phone1 = Phone('989898989')
+    birth1 = Birthday('20.11.1995')
+    rec1 = Record(name1, phone1, birth1)
+
+    name2 = Name('Seee2')
+    phone2 = Phone('989898989')
+    birth2= Birthday('20.11.1995')
+    rec2 = Record(name2, phone2, birth2)
+
+    name3 = Name('Seee3')
+    phone3 = Phone('989898989')
+    birth3 = Birthday('20.11.1995')
+    rec3 = Record(name3, phone3, birth3)
+
+    name4 = Name('Seee4')
+    phone4 = Phone('989898989')
+    birth4 = Birthday('20.11.1995')
+    rec4 = Record(name4, phone4, birth4)
+
+    ab.add_record(rec1)
+    ab.add_record(rec2)
+    ab.add_record(rec3)
+    ab.add_record(rec4)
+    print(ab.data[name.value].birthday)
+    print(ab.iterator(5))
